fba3d7a5a7b527053623ff0c65e1e985240a7ef9
jja2163 Jordi Adoumie <jja2163@columbia.edu>
2022-10-04 16:20:49 +0000

    help

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -8,7 +8,7 @@ class MoviesController < ApplicationController
 
   def index
     @movies = Movie.all
-    @all_ratings = ["PG", "G", "R"]
+    @all_ratings = ['G','PG','PG-13','R']
     #@all_ratings = Movie.all_ratings
   end
 
